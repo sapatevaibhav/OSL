552a5ae96716a99784407ac6c4838d0d9c478ef9
sapate vaibhav <64630274+sapatevaibhav@users.noreply.github.com>
2023-08-21 13:03:28 +0530

    improved

--- pr1.c
+++ pr1.c
@@ -4,56 +4,28 @@
 
 #define MAX_RECORDS 100
 
-struct Employee
-{
+struct Employee {
     int id;
     char name[50];
     char mobile[15];
     float salary;
 };
 
-void clearInputBuffer()
-{
-    while (getchar() != '\n')
-        ;
+void clearInputBuffer() {
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
-void createRecord(FILE *fp)
-{
-    struct Employee emp;
-    printf("ENTER ID OF EMPLOYEE: ");
-    scanf("%d", &emp.id);
-    clearInputBuffer();
-    printf("ENTER NAME OF EMPLOYEE: ");
-    fgets(emp.name, sizeof(emp.name), stdin);
-    emp.name[strlen(emp.name) - 1] = '\0'; // Remove newline character
-    printf("ENTER MOBILE NO. OF EMPLOYEE: ");
-    scanf("%s", emp.mobile);
-    printf("ENTER SALARY OF EMPLOYEE: ");
-    scanf("%f", &emp.salary);
-    fprintf(fp, " %d \t\t%s \t\t %s \t %.2f\n", emp.id, emp.name, emp.mobile, emp.salary);
-}
-
-void viewRecords(FILE *fp)
-{
-    char c;
-    while ((c = fgetc(fp)) != EOF)
-    {
-        printf("%c", c);
-    }
-}
-
-int main()
-{
+int main() {
     char fname[50];
     printf("ENTER ADDRESS BOOK NAME: ");
     scanf("%s", fname);
-    FILE *fp = fopen(fname, "a+");
+    FILE *fp = fopen(fname, "w");
     fprintf(fp, "ID \t\t NAME\t\t MOB_NO\t\t SALARY\n");
+    fclose(fp);
 
     int choice;
-    while (1)
-    {
+    while (1) {
         printf("******ADDRESS BOOK****** :\n");
         printf("\t1.CREATE ADDRESS BOOK\n");
         printf("\t2.VIEW ADDRESS BOOK\n");
@@ -66,31 +38,163 @@ int main()
         scanf("%d", &choice);
         clearInputBuffer();
 
-        switch (choice)
-        {
-        case 1:
-        {
-            printf("Enter number of records you want to enter: ");
-            int n;
-            scanf("%d", &n);
-            clearInputBuffer();
-
-            for (int i = 0; i < n; i++)
-            {
-                createRecord(fp);
+        switch (choice) {
+            case 1: {
+                printf("Enter number of records you want to enter: ");
+                int n;
+                scanf("%d", &n);
+                clearInputBuffer();
+                
+                FILE *fp = fopen(fname, "a");
+                for (int i = 0; i < n; i++) {
+                    struct Employee emp;
+                    printf("ENTER ID OF EMPLOYEE: ");
+                    scanf("%d", &emp.id);
+                    printf("ENTER NAME OF EMPLOYEE: ");
+                    clearInputBuffer();
+                    fgets(emp.name, sizeof(emp.name), stdin);
+                    emp.name[strlen(emp.name) - 1] = '\0'; // Remove newline character
+                    printf("ENTER MOBILE NO. OF EMPLOYEE: ");
+                    scanf("%s", emp.mobile);
+                    printf("ENTER SALARY OF EMPLOYEE: ");
+                    scanf("%f", &emp.salary);
+                    fprintf(fp, " %d \t\t%s \t\t %s \t %.2f\n", emp.id, emp.name, emp.mobile, emp.salary);
+                }
+                fclose(fp);
+                break;
             }
-            break;
-        }
-        case 2:
-            viewRecords(fp);
-            break;
-        // Implement other cases similarly
-        default:
-            printf("PLEASE ENTER THE CORRECT CHOICE\n");
-            break;
+            case 2: {
+                FILE *fp = fopen(fname, "r");
+                char c;
+                while ((c = fgetc(fp)) != EOF) {
+                    printf("%c", c);
+                }
+                fclose(fp);
+                break;
+            }
+            case 3: {
+                struct Employee emp;
+                FILE *fp = fopen(fname, "a");
+                printf("ENTER ID OF EMPLOYEE: ");
+                scanf("%d", &emp.id);
+                printf("ENTER NAME OF EMPLOYEE: ");
+                clearInputBuffer();
+                fgets(emp.name, sizeof(emp.name), stdin);
+                emp.name[strlen(emp.name) - 1] = '\0'; // Remove newline character
+                printf("ENTER MOBILE NO. OF EMPLOYEE: ");
+                scanf("%s", emp.mobile);
+                printf("ENTER SALARY OF EMPLOYEE: ");
+                scanf("%f", &emp.salary);
+                fprintf(fp, " %d \t\t%s \t\t %s \t %.2f\n", emp.id, emp.name, emp.mobile, emp.salary);
+                fclose(fp);
+                break;
+            }
+            case 4: {
+                int eidToDelete;
+                printf("ENTER EMPLOYEE ID TO BE DELETED: ");
+                scanf("%d", &eidToDelete);
+                FILE *inFile = fopen(fname, "r");
+                FILE *outFile = fopen("temp.txt", "w");
+                char line[200];
+                int found = 0;
+                while (fgets(line, sizeof(line), inFile)) {
+                    int id;
+                    sscanf(line, "%d", &id);
+                    if (id != eidToDelete) {
+                        fputs(line, outFile);
+                    } else {
+                        found = 1;
+                    }
+                }
+                fclose(inFile);
+                fclose(outFile);
+                if (found) {
+                    remove(fname);
+                    rename("temp.txt", fname);
+                    printf("RECORD DELETED\n");
+                } else {
+                    remove("temp.txt");
+                    printf("ERROR: RECORD DOES NOT EXIST!!\n");
+                }
+                break;
+            }
+            case 5: {
+                int eidToModify;
+                printf("ENTER EMPLOYEE ID TO BE MODIFIED: ");
+                scanf("%d", &eidToModify);
+                FILE *inFile = fopen(fname, "r");
+                FILE *outFile = fopen("temp.txt", "w");
+                char line[200];
+                int found = 0;
+                while (fgets(line, sizeof(line), inFile)) {
+                    int id;
+                    sscanf(line, "%d", &id);
+                    if (id != eidToModify) {
+                        fputs(line, outFile);
+                    } else {
+                        found = 1;
+                    }
+                }
+                fclose(inFile);
+                fclose(outFile);
+                if (found) {
+                    remove(fname);
+                    rename("temp.txt", fname);
+
+                    struct Employee emp;
+                    FILE *fp = fopen(fname, "a");
+                    emp.id = eidToModify;
+                    printf("ENTER NAME OF EMPLOYEE: ");
+                    clearInputBuffer();
+                    fgets(emp.name, sizeof(emp.name), stdin);
+                    emp.name[strlen(emp.name) - 1] = '\0'; // Remove newline character
+                    printf("ENTER MOBILE NO. OF EMPLOYEE: ");
+                    scanf("%s", emp.mobile);
+                    printf("ENTER SALARY OF EMPLOYEE: ");
+                    scanf("%f", &emp.salary);
+                    fprintf(fp, " %d \t\t%s \t\t %s \t %.2f\n", emp.id, emp.name, emp.mobile, emp.salary);
+                    fclose(fp);
+
+                    printf("RECORD MODIFIED\n");
+                } else {
+                    remove("temp.txt");
+                    printf("ERROR: RECORD DOES NOT EXIST!!\n");
+                }
+                break;
+            }
+            case 6: {
+                int eidToSearch;
+                printf("ENTER EMPLOYEE ID TO BE SEARCHED: ");
+                scanf("%d", &eidToSearch);
+                FILE *fp = fopen(fname, "r");
+                char line[200];
+                int found = 0;
+                while (fgets(line, sizeof(line), fp)) {
+                    int id;
+                    sscanf(line, "%d", &id);
+                    if (id == eidToSearch) {
+                        printf("%s", line);
+                        found = 1;
+                        break;
+                    }
+                }
+                fclose(fp);
+                if (found) {
+                    printf("RECORD FOUND...!!!\n");
+                } else {
+                    printf("ERROR: RECORD DOES NOT EXIST!!!\n");
+                }
+                break;
+            }
+            case 7: {
+                printf("THANK YOU\n");
+                exit(0);
+            }
+            default:
+                printf("PLEASE ENTER THE CORRECT CHOICE\n");
+                break;
         }
     }
 
-    fclose(fp);
     return 0;
 }
